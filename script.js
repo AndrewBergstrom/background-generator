79cfc1b744410f6579b8cf59d2af0456649a85cb
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2021-01-28 15:09:47 -1000

    comments added to clg

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 
+
+// Shows event in console log. Will remove shortly
 color1.addEventListener("input", function(){
   console.log(color1.value)
 })
-
+// Shows event in console log. Will remove shortly
 color2.addEventListener("input", function(){
   console.log(color2.value)
 })
