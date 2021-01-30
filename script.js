f7c3cd9f467d85119e3bde0c7c8a4d815784a95c
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2021-01-29 22:31:47 -1000

    removed comments

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 
 
-// Shows event in console log. Will remove shortly
 color1.addEventListener("input", function(){
   console.log(color1.value)
 })
-// Shows event in console log. Will remove shortly
 color2.addEventListener("input", function(){
   console.log(color2.value)
 })
